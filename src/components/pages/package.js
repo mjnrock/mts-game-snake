62e4811a17fca778d874d597b8880989d7ef7616
Matt Nawrocki <mjnrock@umich.edu>
2020-04-01 15:58:55 -0400

    init

--- src/components/pages/package.js
+++ src/components/pages/package.js
@@ -1,17 +1,11 @@
-import WordChoice from "./WordChoice";
 import Alphabet from "./Alphabet";
-import Winner from "./Winner";
 import Game from "./Game";
 
 import Context from "./../../Context";
-WordChoice.contextType = Context.MTSContext;
 Alphabet.contextType = Context.MTSContext;
-Winner.contextType = Context.MTSContext;
 Game.contextType = Context.MTSContext;
 
 export default {
-    WordChoice,
     Alphabet,
-    Winner,
     Game
 };
